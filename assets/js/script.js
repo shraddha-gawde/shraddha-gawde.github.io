2576f1e3dd98a0df14d6e48cde4c5962ae718e17
shraddha-gawde <shraddha.gawde1999@gmail.com>
2024-02-02 18:01:28 +0530

    portfolio

--- assets/js/script.js
+++ assets/js/script.js
@@ -29,6 +29,55 @@ for (let i = 0; i < navbarLinks.length; i++) {
 
 
 
+// set timer
+const elemTitle = document.querySelector('.elem-title');
+
+    const targetNumber = 500;
+
+    const duration = 10000; 
+
+    const increment = Math.ceil(targetNumber / (duration / 100));
+
+    let currentNumber = 0;
+
+    function updateNumber() {
+        elemTitle.textContent = currentNumber;
+        if (currentNumber < targetNumber) {
+            currentNumber += increment;
+            requestAnimationFrame(updateNumber);
+        }
+    }
+
+    window.addEventListener('load', () => {
+        updateNumber();
+    });
+
+    const elemTitle2 = document.querySelector('.elem-2 .elem-title');
+
+    // Set the target number
+    const targetNumber2 = 1200; // Replace with your desired number
+
+    // Set the duration of the animation in milliseconds
+    const duration2 = 20000; // Adjust as needed
+
+    // Calculate the increment value for each frame
+    const increment2 = Math.ceil(targetNumber2 / (duration2 / 100));
+
+    let currentNumber2 = 0;
+
+    // Function to update the displayed number and schedule the next update
+    function updateNumber2() {
+        elemTitle2.textContent = currentNumber2;
+        if (currentNumber2 < targetNumber2) {
+            currentNumber2 += increment2;
+            requestAnimationFrame(updateNumber2);
+        }
+    }
+
+    // Start the animation when the page is loaded
+    window.addEventListener('load', () => {
+        updateNumber2();
+    });
 
 
 
@@ -50,7 +99,7 @@ window.addEventListener("scroll", function () {
 
 var typed = new Typed(".typing", {
   strings: [
-    "Shraddha Gawde",
+    "Shraddha Subhash Gawde",
     "A Node.js Backend Developer",
     "always eager to learn new things",
     "currently learning about AWS",
